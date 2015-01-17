34557f54eb7010be3b26f887eb165ca8c56ba2e7
Ayush Mehra <ayush.mehra@gmail.com>
2015-01-17 07:22:36 -0500

    Added Parse and necessary frameworks

---
+++ WhatsGood/WhatsGood/EventsListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EventsListViewController.swift
+//  WhatsGood
+//
+//  Created by Ayush Mehra on 1/17/15.
+//  Copyright (c) 2015 amehra. All rights reserved.
+//
+
+import UIKit
+
+class EventsListViewController: UITableViewController {
+    
+    
+    
+    
+}
